403fcd4ff8a53b5260cff041f2075285dd0c4bc1
Behrooz Bozorg Chami <behroozbozorgchami@hotmail.com>
2020-03-08 17:25:10 +0330

    add item compoentent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddItemComponent } from './add-item/add-item.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'addItem',
+    component:AddItemComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
